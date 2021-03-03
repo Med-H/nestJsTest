a2ed48e2ed1daf9748d6ab8f6a7d67a695b1e23c
Med-H <mehdihambli@hotmail.com>
2021-03-03 13:57:09 +0100

    more config and simple nestjs test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
     getHello(): string {
         return this.appService.getHello();
     }
+
+	@Get('ab*cd')
+	getTest(): string {
+		return this.appService.getTest();
+	}
 }
